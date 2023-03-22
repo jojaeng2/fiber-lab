e34db0235d7822bc482ce09f037dc372a0e389dc
jojaeng2 <ds4ouj@naver.com>
2023-03-22 15:55:56 +0900

    feat: Fiber-MySQL connection 생성

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gofiber/fiber/v2 v2.42.0
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -23,6 +24,7 @@ require (
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gorm.io/driver/mysql v1.4.7 // indirect
 	gorm.io/driver/sqlite v1.4.4 // indirect
 	gorm.io/gorm v1.24.6 // indirect
 )
--- server.go
+++ server.go
@@ -5,16 +5,27 @@ import (
 	"custom-modules/repository"
 	"custom-modules/service"
 
-	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	app := fiber.New()
-	userController := initializeUserController()
-	app.Get("/users", userController.FindAllUsers)
-	app.Post("/users", userController.AddUser)
-	app.Get("/users/:email", userController.FindOneByEmail)
-	app.Listen(":3000")
+	// app := fiber.New()
+	// userController := initializeUserController()
+	// app.Get("/users", userController.FindAllUsers)
+	// app.Post("/users", userController.AddUser)
+	// app.Get("/users/:email", userController.FindOneByEmail)
+	// app.Listen(":3000")
+
+	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.Exec("CREATE TABLE IF NOT EXISTS users2 (id INT, name VARCHAR(255))")
+
 }
 
 func initializeUserController() controller.UserController {
